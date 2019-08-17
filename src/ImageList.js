05623f17646b9e7bf42c4a2087d9286872884667
Pat Haynes <pathaynes32@gmail.com>
2019-08-17 11:02:48 -0700

    fixed component issue

--- src/ImageList.js
+++ src/ImageList.js
@@ -3,9 +3,9 @@ import Component from './component/component.js';
 
 class PokemonList extends Component {
     onRender(dom) {
-        const pokemonImages = this.props.pokemonImages;
-        pokemonImages.forEach(pokemonImages => {
-            const props = { pokemonImages: pokemonImages };
+        const pokemon = this.props.pokemon;
+        pokemon.forEach(pokemon => {
+            const props = { pokemon: pokemon };
             const pokeItem = new PokemonItems(props);
             const pokeItemDOM = pokeItem.renderDOM();
             dom.appendChild(pokeItemDOM);
